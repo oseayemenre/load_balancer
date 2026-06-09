39dc4a80dde9ef91dadd9524910789391ae5075c
oseayemenre <oseayemenre04@gmail.com>
2026-06-09 14:25:02 +0100

    refactor

--- internal/server/http.go
+++ internal/server/http.go
@@ -26,7 +26,7 @@ func RegisterRoutes(
 	servers []LbServersConfig,
 	algo string) {
 	var (
-		index       int
+		current     int
 		weightCount = 1
 		mu          sync.Mutex
 	)
@@ -35,54 +35,33 @@ func RegisterRoutes(
 		var req *http.Request
 		var err error
 		var target string
-		var current int
 
 		switch algo {
 		case "ip hash":
-			h := fnv.New32a()
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			current, target, err = ipHash(r.RemoteAddr, servers, r.RequestURI)
 			if err != nil {
-				http.Error(w, fmt.Sprintf("error splitting host port, %v", err), http.StatusBadRequest)
+				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			h.Write([]byte(ip))
-			current = int(h.Sum32()) % len(servers)
-			target = fmt.Sprintf("%s%s", servers[current].Server, r.RequestURI)
 
 		case "least connection":
-			mu.Lock()
-			for i := range servers {
-				if servers[i].Connections < servers[current].Connections {
-					current = i
-				}
-			}
-			servers[current].Connections++
-			target = fmt.Sprintf("%s%s", servers[current].Server, r.RequestURI)
-			mu.Unlock()
-
+			target = leastConnection(&mu, servers, r.RequestURI, current)
 			defer func() {
 				mu.Lock()
 				servers[current].Connections--
 				mu.Unlock()
 			}()
 		default:
-			mu.Lock()
-			current = index
-			target = fmt.Sprintf("%s%s", servers[current].Server, r.RequestURI)
-			if weightCount == servers[current].Weight {
-				weightCount = 1
-				index = (index + 1) % len(servers)
-			} else {
-				weightCount++
-			}
-			mu.Unlock()
+			current, weightCount, target = roundrobin(&mu, servers, current, r.RequestURI, weightCount)
 		}
 
-		if r.Body != nil {
-			req, err = http.NewRequestWithContext(r.Context(), r.Method, target, r.Body)
-		} else {
+		switch r.Method {
+		case http.MethodGet, http.MethodDelete, http.MethodOptions:
 			req, err = http.NewRequestWithContext(r.Context(), r.Method, target, nil)
+		default:
+			req, err = http.NewRequestWithContext(r.Context(), r.Method, target, r.Body)
 		}
+
 		if err != nil {
 			http.Error(w, fmt.Sprintf("error building request, %v", err), http.StatusInternalServerError)
 			return
@@ -105,6 +84,45 @@ func RegisterRoutes(
 
 		writeResponse(w, resp.StatusCode, resp.Header, parsed)
 	})
+}
+
+func roundrobin(mu *sync.Mutex, servers []LbServersConfig, current int, requestURI string, weightCount int) (int, int, string) {
+	mu.Lock()
+	target := fmt.Sprintf("%s%s", servers[current].Server, requestURI)
+	if weightCount == servers[current].Weight {
+		weightCount = 1
+		current = (current + 1) % len(servers)
+	} else {
+		weightCount++
+	}
+	mu.Unlock()
+	return current, weightCount, target
+}
+
+func ipHash(remoteAddr string, servers []LbServersConfig, requestURI string) (int, string, error) {
+	h := fnv.New32a()
+	ip, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return 0, "", fmt.Errorf("error splitting host port, %v", err)
+	}
+	h.Write([]byte(ip))
+	current := int(h.Sum32()) % len(servers)
+	target := fmt.Sprintf("%s%s", servers[current].Server, requestURI)
+	return current, target, nil
+}
+
+func leastConnection(mu *sync.Mutex, servers []LbServersConfig, requestURI string, current int) string {
+	mu.Lock()
+	for i := range servers {
+		if servers[i].Connections/servers[i].Weight < servers[current].Connections/servers[current].Weight {
+			current = i
+		}
+	}
+	servers[current].Connections++
+	mu.Unlock()
+	target := fmt.Sprintf("%s%s", servers[current].Server, requestURI)
+	return target
+
 }
 
 func writeResponse(w http.ResponseWriter, code int, header http.Header, data []byte) {
